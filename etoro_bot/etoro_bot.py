1761002ceef68a065494e8c1b025b4edf22bf9da
Camilo Gomez <caegomezji@gmail.com>
2023-12-27 16:49:20 +0100

    adding etoro bot code

---
+++ etoro_bot/etoro_bot.py
@@ -0,0 +1,315 @@
+#!/usr/bin/python3
+
+import time
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+import undetected_chromedriver as uc
+from selenium import webdriver
+
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import datetime
+from selenium.webdriver import ActionChains
+import os
+import subprocess
+
+from etoro_bot.config import Config
+config = Config()
+
+from enum import Enum
+
+class TradeActions(Enum):
+    buy = 1
+    hold = 0
+    sell = -1
+
+
+#uc.TARGET_VERSION = 85
+
+def load_driver( gui = False):
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+    user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+
+    desired_capabilities = DesiredCapabilities.CHROME.copy()
+    desired_capabilities['user_agent'] = user_agent
+
+    options = webdriver.ChromeOptions()
+    if not gui :
+        options.headless=True
+        options.add_argument("--headless")
+    options.add_argument("--disable-extensions")
+    options.add_argument('--disable-application-cache')
+    options.add_argument('--disable-gpu')
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-setuid-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument('--window-size=1920,1080')
+
+    options.add_argument(f"--user-agent={user_agent}")
+
+    driver = uc.Chrome( version_main = 120 , browser_executable_path="/usr/bin/chromium-browser",
+        desired_capabilities=desired_capabilities, options=options)
+    return driver
+
+
+def login(driver, user, pwd):
+
+    driver.get('https://www.etoro.com/login')
+    #driver.get('http://localhost:9999')
+
+    time.sleep(6)
+    #assert "Facebook" in driver.title
+    print("Logging in...")
+    elem = driver.find_element(By.ID, "username")
+    elem.send_keys(user)
+    elem = driver.find_element(By.ID, "password")
+    elem.send_keys(pwd)
+    time.sleep(1)
+    elem.send_keys(Keys.RETURN)
+    try:
+        wait_ele = EC.presence_of_element_located(
+            (By.CLASS_NAME, 'i-menu-user-username'))
+        elem = driver.find_element(By.CLASS_NAME, "i-menu-user-username")
+    except TimeoutException:
+        print("Loading took too much time!")
+    except:
+        pass
+    if (elem != None) and (elem.text == user):
+        print("logged in")
+    else:
+        print("wtf?")
+    time.sleep(2)
+
+
+def find_elements_by_automation_id(driver_element, automation_id):
+    return driver_element.find_elements(By.CSS_SELECTOR, '[automation-id="{}"]'.format(automation_id))
+
+
+def find_elements_by_data_etoro_automation_id(driver_element, automation_id):
+    return driver_element.find_elements(By.CSS_SELECTOR, '[data-etoro-automation-id="{}"]'.format(automation_id))
+
+
+def close_position(driver, symbols):
+
+    driver.get("https://www.etoro.com/portfolio/positions")
+    time.sleep(6)
+    elements = find_elements_by_automation_id(
+        driver,  "watchlist-grid-instruments-list")
+    assert elements != None, "elements is None"
+    assert len(elements) > 0, "elements is empty"
+    # driver.find_elements
+    print("elements ", elements)
+    order = dict()
+    index = 0
+    for e in elements:
+
+        symbol_div = find_elements_by_automation_id(
+            e,  "portfolio-overview-table-body-cell-market-name")
+        assert symbol_div != None, "symbol_div is None"
+        assert len(symbol_div) > 0, "symbol_div is empty"
+
+        if len(symbol_div) > 0:
+            symbol = symbol_div[0].text
+            order[symbol] = e
+            index += 1
+
+    for symbol in symbols:
+        try:
+            assert symbol in order.keys(), "{} not found".format(symbol)
+
+            buttons = find_elements_by_automation_id(
+                order[symbol], "group-actions-dropdown-3dots")
+            assert len(buttons) == 1, " it brings more than 1 button"
+            button = buttons[0]
+            button.click()
+
+            time.sleep(1)
+
+            dropdowns = find_elements_by_automation_id(
+                driver, "group-actions-instrument-container")
+            assert len(dropdowns) == 1, " it brings more than 1 dropdown"
+            dropdown = dropdowns[0]
+
+            spans = dropdown.find_elements(By.TAG_NAME, "span")
+            assert len(spans) != 0, " it brings more than 1 span"
+
+            for span in spans:
+                print(span.text)
+                if span.text == "Close All":
+                    span.click()
+
+                    time.sleep(1)
+
+                    close_buttons = find_elements_by_data_etoro_automation_id(
+                        driver, "close-all-positions-closs-all-button")
+                    print(close_buttons)
+                    assert len(
+                        close_buttons) == 1, " it brings more than 1 button"
+                    close_button = close_buttons[0]
+
+                    close_button.click()
+
+        except Exception as e:
+            print("error", e)
+            pass
+
+
+def close_position(driver, symbols):
+    driver.get("https://www.etoro.com/portfolio/positions")
+    time.sleep(6)
+    elements = find_elements_by_automation_id(
+        driver,  "watchlist-grid-instruments-list")
+    assert elements != None, "elements is None"
+    assert len(elements) > 0, "elements is empty"
+    # driver.find_elements
+    print("elements ", elements)
+    order = dict()
+    index = 0
+    for e in elements:
+
+        symbol_div = find_elements_by_automation_id(
+            e,  "portfolio-overview-table-body-cell-market-name")
+        assert symbol_div != None, "symbol_div is None"
+        assert len(symbol_div) > 0, "symbol_div is empty"
+
+        if len(symbol_div) > 0:
+            symbol = symbol_div[0].text
+            order[symbol] = e
+            index += 1
+
+    for symbol in symbols:
+        try:
+            assert symbol in order.keys(), "{} not found".format(symbol)
+
+            buttons = find_elements_by_automation_id(
+                order[symbol], "group-actions-dropdown-3dots")
+            assert len(buttons) == 1, " it brings more than 1 button"
+            button = buttons[0]
+            button.click()
+
+            time.sleep(1)
+
+            dropdowns = find_elements_by_automation_id(
+                driver, "group-actions-instrument-container")
+            assert len(dropdowns) == 1, " it brings more than 1 dropdown"
+            dropdown = dropdowns[0]
+
+            spans = dropdown.find_elements(By.TAG_NAME, "span")
+            assert len(spans) != 0, " it brings more than 1 span"
+
+            for span in spans:
+                print(span.text)
+                if span.text == "Close All":
+                    span.click()
+
+                    time.sleep(1)
+
+                    close_buttons = find_elements_by_data_etoro_automation_id(
+                        driver, "close-all-positions-closs-all-button")
+                    print(close_buttons)
+                    assert len(
+                        close_buttons) == 1, " it brings more than 1 button"
+                    close_button = close_buttons[0]
+
+                    close_button.click()
+                    time.sleep(1)
+
+        except Exception as e:
+            print("error", e)
+            pass
+
+
+def open_buy_position(driver, symbol, price):
+    driver.get("https://www.etoro.com/markets/{}/chart".format(symbol.lower()))
+    time.sleep(6)
+
+    trade_button_parents_wraps = find_elements_by_automation_id(
+        driver, "market-page-header-wrapp")
+    assert len(
+        trade_button_parents_wraps) == 1, " it brings more than 1 button wrap"
+    trade_button_parents_wrap = trade_button_parents_wraps[0]
+
+    trade_button_parents_wrap = find_elements_by_automation_id(
+        trade_button_parents_wrap, "market-page-header-trade-button-regular")
+    assert len(trade_button_parents_wrap) == 1, " it brings more than 1 button"
+    trade_button_parent = trade_button_parents_wrap[0]
+
+    trade_buttons = find_elements_by_automation_id(
+        trade_button_parent, "trade-button")
+    print(trade_buttons)
+    assert len(trade_buttons) == 1, " it brings more than 1 button"
+    trade_button = trade_buttons[0]
+    trade_button.click()
+
+    # open the buy/sell menu
+    time.sleep(3)
+
+    value_inputs = find_elements_by_automation_id(driver, "open-position-amount-input-amount")
+    assert len(value_inputs) == 1, " it brings more than 1 value inputs"
+    value_input = value_inputs[0]
+
+    action = ActionChains(driver)
+    action.double_click(value_input).perform()
+
+    value_input.send_keys(price)
+    value_input.send_keys(Keys.RETURN)
+
+    time.sleep(1)
+    entry_order_buttons = find_elements_by_automation_id(
+        driver, "open-position-by-value-submit-button")
+    assert len(entry_order_buttons) == 1, " it brings more than 1 entry button"
+    entry_order_button = entry_order_buttons[0]
+
+    entry_order_button.click()
+
+    time.sleep(1)
+
+
+def go_to_virtual_portfolio(driver):
+
+    virtual_buttons = find_elements_by_automation_id(
+        driver, "sidenav-switch-to-virtual")
+    assert len(virtual_buttons) == 1, " it brings more than 1 virtual button"
+    virtual_button = virtual_buttons[0]
+    virtual_button.click()
+    time.sleep(1)
+
+    toogle_button = driver.find_element(By.CLASS_NAME, "toggle-account-button")
+    assert toogle_button != None, " toogle_button is None"
+    toogle_button.click()
+
+    time.sleep(1)
+
+
+def launch_bot(action :  TradeActions, symbol , gui = False ):
+    user = config.etoro_user
+    pwd = config.etoro_pwd
+    driver = load_driver(gui = gui)
+
+    login(driver, user, pwd)
+    time.sleep(3)
+    go_to_virtual_portfolio(driver)
+    time.sleep(3)
+    value = 50
+
+    if action == TradeActions.hold:
+        close_position(driver, [symbol])
+
+    if action == TradeActions.buy:
+        open_buy_position(driver, symbol, value)
+
+    time.sleep(1)
+
+    csv_file_name = "saves.csv"
+
+    with open(csv_file_name, 'a') as f:
+
+        f.write("{},{},{},{}\n".format(
+            symbol, action, value, datetime.datetime.now()))
+
+
+if __name__ == '__main__':
+    action = TradeActions.buy
+    launch_bot(action , symbol="SPX500" , gui=False)
