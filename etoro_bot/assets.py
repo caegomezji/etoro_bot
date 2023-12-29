83f4c0c88ce7163042772cfb9321c5ff7a981c1e
Camilo Gomez <caegomezji@gmail.com>
2023-12-29 19:02:37 +0000

    adding the telegram sender

---
+++ etoro_bot/assets.py
@@ -0,0 +1,45 @@
+
+import logging
+
+from etoro_bot import config
+import requests
+
+
+class TelegramSender():
+    def __init__(self , telegram_token, telegram_chat_id):
+        self.telegram_token = telegram_token
+        self.telegram_chat_id = telegram_chat_id
+
+    def send_message(self, message):
+
+        send_text = ('https://api.telegram.org/bot' + self.telegram_token + 
+                     '/sendMessage?chat_id=' + self.telegram_chat_id + 
+                     '&parse_mode=Markdown&text=' + message)
+
+        try:
+            response = requests.get(send_text)
+            message = response.json()
+
+        except :
+            message = {
+                    "ok": False ,
+                    "message": "Error on telegram API"
+                }
+        logging.warning(message)
+        return message 
+
+
+telegram = None
+if config.enable_telegram:
+    telegram = TelegramSender(telegram_token = config.telegram_token, telegram_chat_id= config.telegram_chat_id)
+
+
+class BotException(Exception):
+
+    def __init__(self, message):
+        super().__init__(message)
+        logging.error(message)
+        if config.enable_telegram:
+            telegram.send_message(message)
+
+     
--- test/test_config.py
+++ test/test_config.py
@@ -2,12 +2,28 @@
 import unittest
 
 from etoro_bot import config
+from etoro_bot.assets import TelegramSender
+
+
+config.enable_telegram = False
 
 class TestStringMethods(unittest.TestCase):
 
     def test_config(self):
         assert config.etoro_user != None, "Not loaded config values"
         assert config.etoro_pwd != None, "Not loaded config values"
+        
+    @unittest.skipIf( not config.enable_telegram , "Integration Test: Telegram not enabled")
+    def test_send_telegram(self):
+
+        message = "testing telegram integration"
+
+        telegram = TelegramSender(telegram_token=config.telegram_token , telegram_chat_id=config.telegram_chat_id)
+        telegram_response = telegram.send_message(message)
+        assert "ok" in telegram_response , "unrecognizable format"
+        assert telegram_response["ok"], "getting errors on instagram api"
+        
+
 
 if __name__ == '__main__':
     unittest.main()
