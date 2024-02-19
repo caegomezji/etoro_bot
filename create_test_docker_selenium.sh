d1276ffeed93c3811541d7dab636d27d2ae45a39
Camilo Gomez <caegomezji@gmail.com>
2024-02-19 16:42:16 +0100

    creating a test run

--- create_test_docker_selenium.sh
+++ create_test_docker_selenium.sh
@@ -4,10 +4,10 @@ docker kill etoro_bot_test
 docker rm etoro_bot_test
 
 docker build -t caegomezji/spx500_selenium_test .
-
-docker run -dt \
+docker run -it \
 	--restart always \
 	--name=etoro_bot_test \
 	--net=host \
-	--env-file=.env \
-	caegomezji/spx500_selenium_test --port 1888
+	--env-file=.env.test \
+	-v ./:/usr/app \
+	caegomezji/spx500_selenium_test --reload --port 1888
